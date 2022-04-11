dccc9f228d03ab235c914bccbc2d512b6c721009
so <so@DESKTOP-7S3FC6D>
2022-04-11 19:40:34 +0900

    homework

--- HelloWorld/src/co/edu/variable/HomeWork2.java
+++ HelloWorld/src/co/edu/variable/HomeWork2.java
@@ -6,7 +6,12 @@ public class HomeWork2 {
 		int cashe = 10000;
 		int product = 7500;
 		int VAT = (int) (product * 0.1);
+		product = product - VAT;
 		int changes = cashe - (product + VAT);
-		System.out.println();
+		
+		System.out.println("현금 " + cashe + "원");
+		System.out.println("물건 " + product + "원");
+		System.out.println("부가세 " + VAT + "원");
+		System.out.println("잔돈 " + changes + "원");
 	}
 }
