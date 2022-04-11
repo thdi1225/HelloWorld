069a0f05d07dbb8a7797289b80cf222eacf5f607
admin <admin@YD2-09>
2022-04-11 16:37:35 +0900

    알고리즘 공부하자...

--- HelloWorld/src/co/edu/variable/IfExample2.java
+++ HelloWorld/src/co/edu/variable/IfExample2.java
@@ -1,15 +1,24 @@
 package co.edu.variable;
 
+import java.util.Scanner;
+
 public class IfExample2 {
 	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
 		int num1, num2;
-		num1 = 1;
-		num2 = 2;
+		
+		System.out.println("첫번째 정수를 입력하세요.");
+		num1 = scn.nextInt();
+		System.out.println("두번째 정수를 입력하세요.");
+		num2 = scn.nextInt();
+		int result;
 		
 		if(num1 > num2) {
-			System.out.println("큰수는 : " + num1);
+			result = num1-num2;
 		}else {
-			System.out.println("큰수는 : " + num2);
+			result = num2-num1;
 		}
+		
+		System.out.println(result);
 	}
 }
